effba9e466fb5c2d4d16e3f198b3525464ce60b6
Serarinne <160508714+Serarinne@users.noreply.github.com>
2024-04-09 22:16:49 +0700

    Update install-bot.sh

--- wabot/install-bot.sh
+++ wabot/install-bot.sh
@@ -23,6 +23,8 @@ apt install python3-pip
 pip install Flask
 pip install requests
 pip install jsonify
+pip install requests-toolbelt
+pip install dotenv
 
 cat > /etc/systemd/system/wabot.service <<-END
 [Unit]
