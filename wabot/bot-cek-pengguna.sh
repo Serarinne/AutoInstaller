e8e297d4959307df66d7c5f0d9d709540570fa67
Serarinne <160508714+Serarinne@users.noreply.github.com>
2024-09-10 04:26:59 +0700

    Update bot-cek-pengguna.sh

--- wabot/bot-cek-pengguna.sh
+++ wabot/bot-cek-pengguna.sh
@@ -2,3 +2,4 @@
 Total_Akun=$(grep -c -E "^### " "/usr/local/etc/xray/config.json")
 echo -e "Total Akun: ${Total_Akun}"
 grep -E "^### " "/usr/local/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+echo -e "\n"
